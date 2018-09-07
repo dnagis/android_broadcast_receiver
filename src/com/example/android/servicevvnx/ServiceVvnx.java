be521ce92f2479c40820287861fec63f190c1400
dnagis <vincent.achard@gmail.com>
2018-09-07 11:11:09 +0300

    lancement en shell possible: bypass du pb background lancement par whitelisting deviceidle

--- src/com/example/android/servicevvnx/ServiceVvnx.java
+++ src/com/example/android/servicevvnx/ServiceVvnx.java
@@ -1,6 +1,23 @@
 /**
  * Le service le plus simple au monde! Envie de voir si je peux le démarrer en ligne de commande
  * basé sur HelloActivity, et sur la classe de service Alarm dans dev../samples
+ * 
+ * Lancement en shell avec la cmd am (activity manager), avec un intent explicite:
+ * 
+ * am start-service -a android.intent.action.DIAL com.example.android.servicevvnx/.ServiceVvnx
+ * 
+ * Pb si je fais que ça: le système refuse de démarrer avec les msg:
+ * 		en shell:  Error: app is in background uid null
+ * 		en logcat: ActivityManager: Background start not allowed: service Intent { act=android.intent.action.DIAL cmp=com.example.android.servicevvnx/.ServiceVvnx } to com.example.android.servicevvnx/.ServiceVvnx from pid=13083 uid=0 pkg=com.android.shell
+ * * En greppant à partir du msg d'erreur je trouve où ça bloque:
+ * 		frameworks/base/services/core/java/com/android/server/am/ActiveServices.java		--> dans la fonction ActiveServices.startServiceLocked -> li 373 qui appelle:
+ * 		frameworks/base/services/core/java/com/android/server/am/ActivityManagerService.java dans getAppStartModeLocked() li 8677
+ * J'ai franchement pas tout compris à ce qui se passe dans getAppStartModeLocked(), mais en lisant il y avait des mention de whitelisting de deviceidle
+ * (genre + " onwhitelist=" + isOnDeviceIdleWhitelistLocked(uid));) --> du coup comme je sais faire ce whitelisting j'ai essayé et ça marche (miracle)
+ * 
+ * dumpsys deviceidle whitelist +com.example.android.servicevvnx
+ * 
+ * 
  */
 
 package com.example.android.servicevvnx;
